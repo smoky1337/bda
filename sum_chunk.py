5ede13dc36ba1703fd6b308246a1663e3bc52c23
Louis Scheu <louis.scheu@t-online.de>
2022-05-10 12:48:49 +0200

    Create sum_chunk.py

---
+++ sum_chunk.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+in_f, out_f = sys.argv[1:]
+with open(in_f) as inp:
+    lines = inp.readlines()
+    lines =  [s.split("\t") for s in lines]
+    data = pd.DataFrame(lines, columns = ["id","course","grade"])
+    data = data.astype({"id":"int32", "grade":"float"})
+    s = data.groupby("id").agg({"course": "count", "grade": "mean"})
+    print(s[:10])
+with open(out_f, "a+") as out:
+    [out.write(f"{i}\t{c}\t{g}") for i,c,g in zip(s.index, s.values[:,0], s.values[:,1])]
